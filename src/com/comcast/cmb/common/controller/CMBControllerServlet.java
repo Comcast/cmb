a592b4e4e20a64b0d4fd5d38b5a8a3197b2e3f9c
boriwo <mail@boriswolf.com>
2013-09-18 15:54:00 -0700

    uped version

--- src/com/comcast/cmb/common/controller/CMBControllerServlet.java
+++ src/com/comcast/cmb/common/controller/CMBControllerServlet.java
@@ -81,7 +81,7 @@ abstract public class CMBControllerServlet extends HttpServlet {
 	 */
 	public final static ValueAccumulator valueAccumulator = new ValueAccumulator();
 
-	public final static String VERSION = "2.2.36";
+	public final static String VERSION = "2.2.37";
 
 	public volatile static ConcurrentHashMap<String, AtomicLong> callStats;
 	public volatile static ConcurrentHashMap<String, AtomicLong> callFailureStats;
