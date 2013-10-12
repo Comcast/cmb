f3d42778b1eff2d88de792d7803efd49bd18df6c
boriwo <mail@boriswolf.com>
2013-10-12 16:57:33 -0700

    remove dead code

--- src/com/comcast/cqs/model/CQSMessage.java
+++ src/com/comcast/cqs/model/CQSMessage.java
@@ -145,25 +145,6 @@ public final class CQSMessage implements Serializable {
 		this.timebasedId = timebasedId;
 	}
 	
-    public static byte[] computeMD5Hash(InputStream is) throws NoSuchAlgorithmException, IOException {
-        BufferedInputStream bis = new BufferedInputStream(is);
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-            byte[] buffer = new byte[16384];
-            int bytesRead = -1;
-            while ( (bytesRead = bis.read(buffer, 0, buffer.length)) != -1 ) {
-                messageDigest.update(buffer, 0, bytesRead);
-            }
-            return messageDigest.digest();
-        } finally {
-            try {
-                bis.close();
-            } catch ( Exception e ) {
-                System.err.println("Unable to close input stream of hash candidate: " + e);
-            }
-        }
-    }
-	
 	private String getMD5(String message) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		
 		MessageDigest digest = MessageDigest.getInstance("MD5");
