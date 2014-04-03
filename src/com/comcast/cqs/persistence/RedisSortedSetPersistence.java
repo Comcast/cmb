ad2880c0ef989b5e3f31673e65e98cb91dee9a28
boriwo <mail@boriswolf.com>
2014-04-03 11:59:05 -0700

    added some basic jedis stuff

--- src/com/comcast/cqs/persistence/RedisSortedSetPersistence.java
+++ src/com/comcast/cqs/persistence/RedisSortedSetPersistence.java
@@ -18,18 +18,100 @@ package com.comcast.cqs.persistence;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.apache.log4j.Logger;
 import org.json.JSONException;
 
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedisPool;
+
+import com.comcast.cmb.common.util.CMBProperties;
 import com.comcast.cmb.common.util.PersistenceException;
 import com.comcast.cqs.model.CQSMessage;
 import com.comcast.cqs.model.CQSQueue;
 
 public class RedisSortedSetPersistence implements ICQSMessagePersistence, ICQSMessagePersistenceIdSequence {
 	
-	//TODO: cache filler
+    private static final Logger logger = Logger.getLogger(RedisSortedSetPersistence.class);
+    private static final Random rand = new Random();
+    
+    private static RedisSortedSetPersistence instance;
+    
+    public static ExecutorService executor;
+    
+    private static JedisPoolConfig config = new JedisPoolConfig();
+    private static ShardedJedisPool pool;
+    static {
+        initializeInstance();
+        initializePool();
+    }
+    
+    private static void initializeInstance() {
+        CQSMessagePartitionedCassandraPersistence cassandraPersistence = new CQSMessagePartitionedCassandraPersistence();
+        instance = new RedisSortedSetPersistence(cassandraPersistence);            
+    }
+    
+    private volatile ICQSMessagePersistence payloadPersistence; 
+    
+    private RedisSortedSetPersistence(ICQSMessagePersistence persistenceStorage) {
+        this.payloadPersistence = persistenceStorage;
+    }
+    /**
+     * Initialize the Redis connection pool
+     */
+    private static void initializePool() {
+    	
+        config.maxActive = CMBProperties.getInstance().getRedisConnectionsMaxActive();
+        config.maxIdle = -1;
+        List<JedisShardInfo> shardInfos = new LinkedList<JedisShardInfo>();
+        String serverList = CMBProperties.getInstance().getRedisServerList();
+        
+        if (serverList == null) {
+            throw new RuntimeException("Redis server list not specified");
+        }
+        
+        String []arr = serverList.split(",");
+        
+        for (int i = 0; i < arr.length; i++) {
+            
+        	String []hostPort = arr[i].trim().split(":");
+        	JedisShardInfo shardInfo = null;
+
+        	if (hostPort.length != 2) {
+        		// use Redis default port if one wasn't specified
+        		 shardInfo = new JedisShardInfo(hostPort[0].trim(), 6379, 4000);
+            } else {
+            	 shardInfo = new JedisShardInfo(hostPort[0].trim(), Integer.parseInt(hostPort[1].trim()), 4000);
+            }
+            
+        	shardInfos.add(shardInfo);
+        }
+        
+        pool = new ShardedJedisPool(config, shardInfos);
+        executor = Executors.newFixedThreadPool(CMBProperties.getInstance().getRedisFillerThreads());
+        
+        logger.info("event=initialize_redis pools_size=" + shardInfos.size() + " max_active=" + config.maxActive + " server_list=" + serverList);
+    }
+    
+    /**
+     * Possible states for queue
+     * State if Unavailable should be set when any code determines the queue is in bad state or unavailable.
+     * The checkCacheConsistency will take care of performing the appropriate actions on that queue
+     */
+    public enum QCacheState {
+        Filling, //Cache is being filled by a thread 
+        OK, //Cache is good for use 
+        Unavailable; //Cache is unavailable for a single Q due to inconsistency issues.
+    }
+
+    //TODO: cache filler
 	
 	//TODO: cache consistency check
 
@@ -120,5 +202,4 @@ public class RedisSortedSetPersistence implements ICQSMessagePersistence, ICQSMe
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
