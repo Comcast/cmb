686528a432ca2d3efc9f5c93458f17d70743632d
Bill Hathaway <william_hathaway@cable.comcast.com>
2014-05-13 15:23:48 -0700

    move schema in travis

--- bin/travisci_setup.sh
+++ bin/travisci_setup.sh
@@ -4,7 +4,11 @@ echo "*** Finished mvn"
 echo "*** Starting untar into tmp"
 tar -xvz -C /tmp -f target/cmb-distribution-*
 echo "*** Finished untar into tmp"
-cp /tmp/cmb/config/test.log4j.properties /tmp/cmb/config/log4j.properties
+
+#cp /tmp/cmb/config/test.log4j.properties /tmp/cmb/config/log4j.properties
+
+echo "installing schema"
+cassandra-cli -f /tmp/cmb/schema/cassandra_1.2.schema && sleep 5
 (cd /tmp/cmb && nohup bin/cmb.sh &)
 echo "*** Ran cmb.sh"
 sleep 5
